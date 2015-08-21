0dba1d7cf5cfbaebd5955c3b94739c81fb571e92
5731083221-PL <pee.luang45@gmail.com>
2015-08-21 07:23:21 +0700

    Implement Greeting Message Implement Bubblesort

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,11 +15,21 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "Hello";
+		return "Hello GitHub";
 	}
 	
 	public static int[] bubbleSort(int[] array){
 		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
+		for(int i=0;i<array.length-1;i++){
+			for(int j=i+1;j<array.length;j++){
+				if(array[i]>array[j]){
+					int temp;
+					temp=array[i];
+					array[i]=array[j];
+					array[j]=temp;
+				}
+			}
+		}
 		return array;
 	}
 	
